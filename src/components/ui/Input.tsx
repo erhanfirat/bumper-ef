94ab1825ca55ffafc7bb0448bbe1658befba5e06
Erhan FIRAT <erhan.firat@cytosmart.com>
2025-04-08 14:55:34 +0300

    Input component created

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,28 @@
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  error?: string;
+  className?: string;
+};
+
+export default function Input({
+  type = "text",
+  onChange,
+  error,
+  className = "",
+  ...htmlInputAttrs
+}: InputProps) {
+  return (
+    <>
+      <input
+        type={type}
+        onChange={onChange}
+        className={`border rounded-full px-4 py-2 focus:outline-none 
+          focus:ring-2 focus:ring-green-500 ${
+            error ? "border-red-500" : "border-black"
+          } ${className}`}
+        {...htmlInputAttrs}
+      />
+      {error && <span className="text-red-500 text-sm mt-1">{error}</span>}
+    </>
+  );
+}
