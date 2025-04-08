7d617351445d904a7192e539a5d19fc23fbdc4ba
Erhan FIRAT <erhan.firat@cytosmart.com>
2025-04-08 11:02:18 +0300

    Hero Star icon and component created and added to

--- src/components/shared/Hero.tsx
+++ src/components/shared/Hero.tsx
@@ -1,12 +1,54 @@
 import Link from "next/link";
 import Button from "../ui/Button";
+import Image from "next/image";
+import Star from "../icons/Star";
 
 export default function Hero() {
   return (
-    <section className="bg-[url(/images/hero-bg.jpeg)] bg-cover bg-center py-16 relative">
+    <section className="bg-[url(/images/hero-bg.jpeg)] bg-cover bg-center py-20 relative">
       <div className="absolute inset-0 bg-black opacity-50"></div>
       <div className="container mx-auto px-4 z-2 relative">
         <div className="flex flex-col items-start">
+          <p className="flex space-x-3 items-center text-white text-lg font-semibold mb-2 py-4">
+            <span>Excellent</span>
+            <div className="flex space-x-0.5">
+              <Star
+                className="bg-green-500 fill-white p-0.5"
+                width={22}
+                height={22}
+              />
+              <Star
+                className="bg-green-500 fill-white p-0.5"
+                width={22}
+                height={22}
+              />
+              <Star
+                className="bg-green-500 fill-white p-0.5"
+                width={22}
+                height={22}
+              />
+              <Star
+                className="bg-green-500 fill-white p-0.5"
+                width={22}
+                height={22}
+              />
+              <Star
+                className="bg-green-500 fill-white p-0.5"
+                width={22}
+                height={22}
+              />
+              <Star
+                className="bg-green-500 fill-white p-0.5"
+                width={22}
+                height={22}
+              />
+            </div>
+
+            <span className="flex items-center">
+              <Image src="/icons/star.svg" alt="" width={20} height={20} />{" "}
+              <span className="font-normal">Trustpilot</span>
+            </span>
+          </p>
           <h1 className="text-4xl md:text-7xl font-oswald font-bold text-white dark:text-white mb-4">
             BECOME A BUMPER APPROVED DEPENDABLE DEALERSHIP
           </h1>
