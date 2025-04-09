03c70d03e5a5f271bc3631e2da98db00b647bbfc
Erhan FIRAT <erhan.firat@cytosmart.com>
2025-04-09 22:09:07 +0300

    api fetcher created

---
+++ src/lib/api.ts
@@ -0,0 +1,23 @@
+const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+export async function fetcher<T>(
+  url: string,
+  options?: RequestInit
+): Promise<T> {
+  try {
+    const res = await fetch(`${baseUrl}${url}`, options);
+
+    if (!res.ok) {
+      const errorBody = await res.json();
+      throw new Error(errorBody?.error || "API Error");
+    }
+
+    return await res.json();
+  } catch (error) {
+    if (error) {
+      console.error("Fetch error:", error);
+      throw error;
+    }
+    throw new Error("An unknown error occurred");
+  }
+}
