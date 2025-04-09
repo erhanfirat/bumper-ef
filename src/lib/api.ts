3417ddb7a660b0afefb23130d4129abcbe39aef5
Erhan FIRAT <erhan.firat@cytosmart.com>
2025-04-10 02:01:12 +0300

    default base url added on case missing env file

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,5 @@
-const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+const baseUrl =
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api";
 
 export async function fetcher<T>(
   url: string,
